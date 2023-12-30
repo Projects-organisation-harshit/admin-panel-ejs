121d2fb0c0cc9aadd367121498a0cb25d6412cb4
harshit0571 <harshitchadha65@gmail.com>
2023-12-30 23:08:05 +0530

    news crud done

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -25,4 +25,39 @@ router.post("/add", (req, res) => {
   );
 });
 
+router.get("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  const query = "delete from news where id=?";
+  connection.query(query, [id], (err) => {
+    if (err) throw err;
+
+    res.redirect("/news");
+  });
+  connection.query("ALTER TABLE news AUTO_INCREMENT = 1;");
+});
+
+router.get("/edit/:id", (req, res) => {
+  const id = req.params.id;
+  const selectQuery = "SELECT * FROM news WHERE id = ?";
+  connection.query(selectQuery, [id], (err, results) => {
+    if (err) throw err;
+    res.render("editNews", { news: results[0] });
+  });
+});
+
+router.post("/update/:id", (req, res) => {
+  const id = req.params.id;
+  const { title, description, photourl, articlelink, date } = req.body;
+  const updateQuery =
+    "UPDATE news SET title=?, description=?, photourl=?, articlelink=?, date=? WHERE id = ?";
+  connection.query(
+    updateQuery,
+    [title, description, photourl, articlelink, date, id],
+    (err, results) => {
+      if (err) throw err;
+      res.redirect("/news");
+    }
+  );
+});
+
 module.exports = router;
