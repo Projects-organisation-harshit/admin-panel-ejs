9f23a4b3016c33dc3f53c0a75687dbdea016f20b
harshit0571 <harshitchadha65@gmail.com>
2023-12-30 18:27:35 +0530

    update and delete event done

--- routes/eventsRoutes.js
+++ routes/eventsRoutes.js
@@ -22,4 +22,38 @@ router.post("/add", (req, res) => {
     }
   );
 });
+
+router.get("/delete/:id", (req, res) => {
+  const userId = req.params.id;
+  const deleteQuery = "DELETE FROM events WHERE id = ?";
+  connection.query(deleteQuery, [userId], (err, results) => {
+    if (err) throw err;
+    res.redirect("/events");
+  });
+});
+
+router.get("/edit/:id", (req, res) => {
+  const userId = req.params.id;
+  const selectQuery = "SELECT * FROM events WHERE id = ?";
+  connection.query(selectQuery, [userId], (err, results) => {
+    if (err) throw err;
+    res.render("edit", { event: results[0] });
+  });
+});
+
+router.post("/update/:id", (req, res) => {
+  const eventId = req.params.id;
+  const { title, description, address, city, date } = req.body;
+  const updateQuery =
+    "UPDATE events SET title = ?, description = ?, address = ?, city = ?, date = ? WHERE id = ?";
+  connection.query(
+    updateQuery,
+    [title, description, address, city, date, eventId],
+    (err, results) => {
+      if (err) throw err;
+      res.redirect("/events");
+    }
+  );
+});
+
 module.exports = router;
