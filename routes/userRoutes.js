79b7fedbaf6896d803561a3fdac60abfbd15048a
harshit0571 <harshitchadha65@gmail.com>
2024-01-08 20:41:40 +0530

    login added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -106,6 +106,32 @@ router.post("/update/:id", async (req, res) => {
   );
 });
 
-router.post("/login", (req, res) => {});
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  connection.query(
+    "SELECT * FROM users WHERE username = ?",
+    [username],
+    (err, results) => {
+      if (err) {
+        console.error("Error querying database:", err);
+        return res.redirect("/user/login");
+      }
+
+      const user = results[0];
+
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.userId = user.id;
+        res.redirect("/");
+      } else {
+        res.redirect("/user/login");
+      }
+    }
+  );
+});
 
 module.exports = router;
