8ef6768a951210a29d32baf6328a77743a939d4f
harshit0571 <harshitchadha65@gmail.com>
2024-02-29 12:15:02 +0530

    added rds

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "database-1.czw6qum40gpe.us-east-1.rds.amazonaws.com",
+  port: 3306,
   user: "root",
-  password: "password",
-  database: "ngowebsite",
+  password: "12345678",
+  database: "ngo",
 });
 
 module.exports = connection;
